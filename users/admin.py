55545151f6253db5dc7212300d8ec620ba235538
ArthurMouraSales <arthur.mourasales@gmail.com>
2025-11-18 00:44:56 -0300

    correções models| users adminpt2

--- users/admin.py
+++ users/admin.py
@@ -8,20 +8,41 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
 
-    list_display = ['email', 'nome_completo', 'cpf', 'data_nascimento', 'contato', 'status', 'is_staff', 'is_active']
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('email', 'password')}),
-        ('Informações Pessoais', {'fields': ('nome_completo', 'cpf', 'data_nascimento', 'contato', 'nome_social', 'genero', 'nome_responsavel')}),
-        ('Permissões', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
-        ('Status do Usuário', {'fields': ('status',)}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets +(
+    list_display = ['email', 'nome_completo', 'cpf', 'is_staff']
+    
+    #fields used in the user creation page
+    add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'nome_completo', 'cpf', 'data_nascimento', 'contato', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
+            'fields': ('email', 'nome_completo', 'cpf', 'data_nascimento', 'contato', 'password', 'password2'),
+        }),
     )
-    
-admin.site.register(CustomUser, CustomUserAdmin)
 
+    #fiels used in the user edit page
+    fieldsets = (
+
+        (None, {'fields': ('password',)}), 
+        ('Informações Pessoais', {'fields': ( 
+            'email', 
+            'nome_completo', 
+            'nome_social',
+            'cpf', 
+            'data_nascimento', 
+            'contato'
+        )}),
+        ('Permissões', {'fields': ( #permissions section
+            'is_active', 
+            'is_staff', 
+            'is_superuser', 
+            'groups', 
+            'user_permissions'
+        )}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}), # Seção de datas (padrão)
+    )
+
+    #fields using to filter users in admin
+    search_fields = ('email', 'nome_completo', 'cpf')
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
