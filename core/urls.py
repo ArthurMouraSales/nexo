cb57e351802209a03077a6a334be982f0d7487eb
ArthurMouraSales <arthur.mourasales@gmail.com>
2025-11-18 02:25:28 -0300

    add teste relatorio

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from users.views import CustomLoginView, SignUpView 
-from nexo.views import DashboardView 
+from nexo.views import DashboardView, CalendarioView, RelatorioPDFView
 from django.contrib.auth.views import LogoutView 
 
 urlpatterns = [
@@ -13,6 +13,11 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     #Dashboard route
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    #Calendar route
+    path('calendario/', CalendarioView.as_view(), name='calendario'),
+    #PDF Report route
+    path('relatorio/download/', RelatorioPDFView.as_view(), name='relatorio_pdf'),
     #Admin
     path('admin/', admin.site.urls),
 ]
+
--- nexo/views.py
+++ nexo/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from weasyprint import HTML
+from .models import RegistroCalendario
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'nexo/dashboard.html'  
+
+class RelatorioPDFView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        profile = user.perfil
+        registros = RegistroCalendario.objects.filter(paciente=user)
+        total_dias_registrados = registros_calendario.count()
+
+    contexto = {
+            'usuario': user,
+            'perfil': profile,
+            'total_dias_registrados': total_dias_registrados,
+            'registros': registros_calendario[:10] 
+        }
+
+    html_string = render_to_string('nexo/relatorio_template.html', contexto)
+    pdf = HTML(string=html_string).write_pdf()
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="relatorio_{user.email}.pdf"'
+
+    return response
