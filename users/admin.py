0ddc87b6cba6b67f46a1b1fd1ecdf83ca4aeca4c
ArthurMouraSales <arthur.mourasales@gmail.com>
2025-11-18 00:39:51 -0300

    correções models| users admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ['email', 'nome_completo', 'cpf', 'data_nascimento', 'contato', 'status', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('email', 'password')}),
+        ('Informações Pessoais', {'fields': ('nome_completo', 'cpf', 'data_nascimento', 'contato', 'nome_social', 'genero', 'nome_responsavel')}),
+        ('Permissões', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
+        ('Status do Usuário', {'fields': ('status',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets +(
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nome_completo', 'cpf', 'data_nascimento', 'contato', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    
+admin.site.register(CustomUser, CustomUserAdmin)
+
--- users/models.py
+++ users/models.py
@@ -1,5 +1,26 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, nome_completo, cpf, data_nascimento, contato, password=None, **extra_fields):
+        if not email:
+            raise ValueError('O email deve ser fornecido')
+        email = self.normalize_email(email)
+        user = self.model(email=email, nome_completo=nome_completo, cpf=cpf, data_nascimento=data_nascimento, contato=contato, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nome_completo, cpf, data_nascimento, contato, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, nome_completo, cpf, data_nascimento, contato, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     username = None
@@ -16,7 +37,12 @@ class CustomUser(AbstractUser):
     status_choices = [ ('ativo', 'Ativo'), ('desativado', 'Desativado'), ('suspenso', 'Suspenso'), ('cancelado', 'Cancelado'),]
     status = models.CharField(max_length=20, choices=status_choices, default='ativo')
 
+    objects = CustomUserManager()
+
     #email is the login field
     USERNAME_FIELD = 'email'
 
     REQUIRED_FIELDS = ['nome_completo', 'cpf', 'data_nascimento', 'contato', 'nome_social', 'genero', 'nome_responsavel']
+
+    def __str__(self):
+        return self.email
